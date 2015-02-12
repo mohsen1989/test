41f87f2a62ed6fa9491732fff05483cd7ee3e35b
Alireza <Alireza.Rahimian91@gmail.com>
2015-02-12 17:04:59 +0330

    Cube Rotated!

---
+++ Assets/_Test/Rotator.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotator : MonoBehaviour
+{
+	public float duration = 1f;
+	public float Angle = 30f;
+	float start_time=0;
+	// Use this for initialization
+	void Start ()
+	{
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if(start_time+duration<Time.time)
+		{
+			start_time = Time.time;
+			RotateMe();
+		}
+	}
+
+	void RotateMe()
+	{
+		transform.Rotate (Vector3.up, Angle);
+	}
+}
