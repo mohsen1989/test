532c52ddbb2e57c28bfba20927a1cc4428d5bb28
mohsen <mohsen.bahrami1989@hotmail.com>
2015-02-14 14:08:06 +0330

    i did not do that

---
+++ Assets/movemjent.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class movemjent : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey(KeyCode.A))
+			transform.localScale = new Vector3 (10, 10, 10);
+		if (Input.GetKey(KeyCode.D))
+			transform.localScale = new Vector3 (1, 1, 1);	
+	
+	}
+}
